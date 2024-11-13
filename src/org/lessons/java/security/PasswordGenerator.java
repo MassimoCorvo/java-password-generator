5208b5018d64ba91d4d14b4c2812aa74d18bbafd
MassimoCorvo <massimocorvo22@gmail.com>
2024-11-13 18:28:21 +0100

    Password generator with  input

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,21 +1,36 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        
-        String nome = "Paolo";
-        String cognome = "Rossi";
-        String colorePreferito = "Rosso";
-        int giornoNascita = 20;
-        int meseNascita = 10;
-        int annoNascita = 1950;
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Inserisci il nome e premi invio");
+        String nome = inputScanner.nextLine();
+
+        System.out.println("Inserisci il cognome e premi invio");
+        String cognome = inputScanner.nextLine();
+
+        System.out.println("Inserisci il colore preferito e premi invio");
+        String colorePreferito = inputScanner.nextLine();
+
+        System.out.println("Inserisci il giorno di nascita e premi invio");
+        int giornoNascita = inputScanner.nextInt();
+
+        System.out.println("Inserisci il mese di nascita e premi invio");
+        int meseNascita = inputScanner.nextInt();
+
+        System.out.println("Inserisci l'anno di nascita e premi invio");
+        int annoNascita = inputScanner.nextInt();
 
         int somma = giornoNascita + meseNascita + annoNascita;
 
         String password = nome + "-" + cognome + "-" + colorePreferito + "-" + somma;
         System.out.println(password);
 
-
+        inputScanner.close();
 
     }
 }
